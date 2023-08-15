951a8066043e4caf8c249cb365eaa559c1150794
Nikita <nnazarov55@gmail.com>
2023-08-15 12:56:12 -0700

    create TransactionHistory Model

---
+++ server/src/migrations/20230815174747-create-transaction-history.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('transaction_histories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      operationType: {
+        field:"operation_type",
+        type: DataTypes.ENUM('INCOME', 'CONSUMPTION'),
+        allowNull: false,
+      },
+      sum: {
+        type: DataTypes.FLOAT,
+    allowNull: false,
+      },
+      userId: {
+        field:"user_id",
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      },
+      createdAt: {
+        field:"created_at",
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        field:"updated_at",
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('transaction_histories');
+  }
+};
