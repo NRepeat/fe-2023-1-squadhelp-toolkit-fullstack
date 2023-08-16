19f1403d38fdc2e80e9ba22a8431c558a14392ed
Nikita <nnazarov55@gmail.com>
2023-08-16 08:49:37 -0700

    fix db name

--- server/src/middlewares/basicMiddlewares.js
+++ server/src/middlewares/basicMiddlewares.js
@@ -21,7 +21,7 @@ module.exports.canGetContest = async (req, res, next) => {
   const { params: { contestId } } = req;
   try {
     if (req.tokenData.role === CONSTANTS.CUSTOMER) {
-      result = await bd.Contests.findOne({
+      result = await db.Contests.findOne({
         where: { id:contestId, userId: req.tokenData.userId },
       });
     } else if (req.tokenData.role === CONSTANTS.CREATOR) {
