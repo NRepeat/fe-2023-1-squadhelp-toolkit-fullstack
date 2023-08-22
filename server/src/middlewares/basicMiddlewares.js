4954a1ce0e43d4afe02fa279d28f36bd35654d69
Nikita <nnazarov55@gmail.com>
2023-08-23 01:23:45 +0300

    fix parseBody

--- server/src/middlewares/basicMiddlewares.js
+++ server/src/middlewares/basicMiddlewares.js
@@ -4,13 +4,12 @@ const ServerError = require('../errors/ServerError');
 const CONSTANTS = require('../constants');
 
 module.exports.parseBody = (req, res, next) => {
-	const {files,body:{contests}}=req
-  contests = JSON.parse(contests);
-  for (let i = 0; i < contests.length; i++) {
-    if (contests[i].haveFile) {
-      const file = files.splice(0, 1);
-      contests[i].fileName = file[0].filename;
-      contests[i].originalFileName = file[0].originalname;
+  req.body.contests = JSON.parse(req.body.contests);
+  for (let i = 0; i < req.body.contests.length; i++) {
+    if (req.body.contests[i].haveFile) {
+      const file = req.files.splice(0, 1);
+      req.body.contests[i].fileName = file[0].filename;
+      req.body.contests[i].originalFileName = file[0].originalname;
     }
   }
   next();
@@ -18,7 +17,6 @@ module.exports.parseBody = (req, res, next) => {
 
 module.exports.canGetContest = async (req, res, next) => {
   let result = null;
-  const { params: { contestId } } = req;
   try {
     const { params: { contestId } } = req;
     if (req.tokenData.role === CONSTANTS.CUSTOMER) {
