f3b8762bfa6bc633362d2847546719f504e303c5
Nikita <nnazarov55@gmail.com>
2023-08-27 21:53:22 +0300

    How it Works page

--- client/src/components/HowItWorks/Questions.jsx
+++ client/src/components/HowItWorks/Questions.jsx
@@ -1,7 +1,75 @@
-import React from 'react'
-
+import React from 'react';
+import styles from './style.module.scss';
+import constants from '../../constants';
 export default function Questions() {
-	return (
-		<div>Questions</div>
-	)
+  const ForbesImg = ({ ...props }) => (
+    <img className={styles.stars} {...props} />
+  );
+  ForbesImg.defaultProps = {
+    src: `${constants.STATIC_IMAGES_PATH}/howItwork/forbes.svg`,
+    alt: 'ForbesImg',
+  };
+  const ChicagoImg = ({ ...props }) => (
+    <img className={styles.stars} {...props} />
+  );
+  ChicagoImg.defaultProps = {
+    src: `${constants.STATIC_IMAGES_PATH}/howItwork/chicago.svg`,
+    alt: 'ChicagoImg',
+  };
+  const MashableImg = ({ ...props }) => (
+    <img className={styles.stars} {...props} />
+  );
+  MashableImg.defaultProps = {
+    src: `${constants.STATIC_IMAGES_PATH}/howItwork/Mashable.svg`,
+    alt: 'MashableImg',
+  };
+  const TNWImg = ({ ...props }) => <img className={styles.stars} {...props} />;
+  TNWImg.defaultProps = {
+    src: `${constants.STATIC_IMAGES_PATH}/howItwork/TNW.svg`,
+    alt: 'TNWImg',
+  };
+  return (
+    <>
+      {' '}
+      <div className={styles.Qcontainer}>
+        <div className={styles.Qwrapper}>
+          <div className={styles.QblockWrapper}>
+            <span className={styles.Qblock}>
+              <h2>Pay a Fraction of cost vs hiring an agency</h2>
+              <p>
+                For as low as $199, our naming contests and marketplace allow
+                you to get an amazing brand quickly and affordably.
+              </p>
+            </span>
+            <span className={styles.Qblock}>
+              <h3>Satisfaction Guarantee</h3>
+              <p>
+                Of course! We have policies in place to ensure that you are
+                satisfied with your experience. <a href="#">Learn more</a>
+              </p>
+            </span>
+          </div>
+          <div className={styles.Qq}>
+            <h1>Questions?</h1>
+            <p>
+              Speak with a Squadhelp platform expert to learn more and get your
+              questions answered.
+            </p>
+            <button>Schedule Consultation</button>
+            <p> (877) 355-3585</p>
+            <p>Call us for assistance</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.Logocontainer}>
+        Featured In
+        <div className={styles.Logowrapper}>
+          <ForbesImg />
+          <TNWImg />
+          <ChicagoImg />
+          <MashableImg />
+        </div>
+      </div>
+    </>
+  );
 }
--- client/src/components/HowItWorks/ReadyGetStarted.jsx
+++ client/src/components/HowItWorks/ReadyGetStarted.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
-
+import React from 'react';
+import styles from './style.module.scss';
+import constants from '../../constants';
 export default function ReadyGetStarted() {
-	return (
-		<div>Readygetstarted</div>
-	)
+  const StarsImg = ({ ...props }) => (
+    <img className={styles.stars} {...props} />
+  );
+  StarsImg.defaultProps = {
+    src: `${constants.STATIC_IMAGES_PATH}/howItwork/stars.svg`,
+    alt: 'StarsImg',
+  };
+  const PeopleImg = ({ ...props }) => (
+    <img className={styles.stars} {...props} />
+  );
+  PeopleImg.defaultProps = {
+    src: `${constants.STATIC_IMAGES_PATH}/howItwork/people.webp`,
+    alt: 'PeopleImg',
+  };
+  const SharingfilesImg = ({ ...props }) => (
+    <img className={styles.stars} {...props} />
+  );
+  SharingfilesImg.defaultProps = {
+    src: `${constants.STATIC_IMAGES_PATH}/howItwork/sharing-files.svg`,
+    alt: 'SharingfilesImg',
+  };
+  return (
+    <div className={styles.RTGScontainer}>
+      <div className={styles.RTGSwrapper}>
+        <div className={styles.RTGSbaner}>
+          <h1 className={styles.RTGSh1}>Ready to get started?</h1>
+          <p className={styles.RTGSbanerP}>
+            Fill out your contest brief and begin receiving custom name
+            suggestions within minutes.
+          </p>
+          <button className={styles.RTGSbutton}>Start A Contest</button>
+        </div>
+        <div className={styles.RTGScards}>
+          <span className={styles.RTGScard}>
+            <StarsImg />
+            <p>
+              <span className={styles.RTGScaradboldfont}>
+                4.9 out of 5 stars
+              </span>{' '}
+              from 25,000+ customers.
+            </p>
+          </span>
+          <span className={styles.RTGScard}>
+            <PeopleImg />
+            <p>
+              Our branding community stands{' '}
+              <span className={styles.RTGScaradboldfont}>200,000+</span> strong.
+            </p>
+          </span>
+          <span className={styles.RTGScard}>
+            <SharingfilesImg />
+            <p>
+              <span className={styles.RTGScaradboldfont}>140+ Industries</span>{' '}
+              supported across more than
+              <span className={styles.RTGScaradboldfont}>85 countries</span> –
+              and counting.
+            </p>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
