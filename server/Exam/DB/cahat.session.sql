b5858502baf0f1944091745a1c82348156853db3
Nikita <nnazarov55@gmail.com>
2023-08-30 15:35:53 +0300

    db req exam

--- server/Exam/DB/cahat.session.sql
+++ server/Exam/DB/cahat.session.sql
@@ -25,3 +25,39 @@ CREATE TABLE catalog_chats (
 SELECT role, COUNT(*) AS quantity
 FROM "Users"
 GROUP BY role;
+
+INSERT INTO "Contests" (
+  "contestType", "fileName", "originalFileName", title, "typeOfName", industry, "focusOfWork",
+  "targetCustomer", "styleName", "nameVenture", "typeOfTagline", status, "brandStyle", prize,
+  "createdAt", priority, "orderId", "userId"
+)
+VALUES (
+  'name', 'name.png', 'name_original.png', 'Sample Contest', 'Type of Name',
+  'Sample Industry', 'Sample Focus of Work', 'Sample Target Customer',
+  'Sample Style Name', 'Sample Name Venture', 'Type of Tagline',
+  'active', 'Sample Brand Style', 1000.00,
+  NOW(), 1, 'ORD123', 1
+);
+
+
+UPDATE "Users" AS u 
+SET balance = u.balance + (os.total_prize * 0.1)
+FROM (
+  SELECT c."userId" AS user_id, SUM(c.prize) AS total_prize
+  FROM "Contests" c
+  WHERE c."createdAt" >= '2022-12-25' AND c."createdAt" <= '2023-09-14'
+  GROUP BY c."userId"
+) AS os
+WHERE u.id = os.user_id AND u.role = 'customer';
+
+
+UPDATE "Users" AS u
+SET balance = balance + 10
+FROM (
+  SELECT id
+  FROM "Users"
+  WHERE role = 'creator'
+  ORDER BY rating DESC
+  LIMIT 3
+) AS top_creative
+WHERE u.id = top_creative.id;
